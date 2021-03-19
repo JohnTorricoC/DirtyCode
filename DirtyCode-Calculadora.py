703b7b5dd19a62d3584d5ea0448f132f1e76beb1
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-03-18 20:17:24 -0400

    Update DirtyCode-Calculadora.py

--- DirtyCode-Calculadora.py
+++ DirtyCode-Calculadora.py
@@ -1,16 +1,23 @@
 def calculadora(r):
-    
-    n1=int(input("Digito 1: "))
     n2=int(input("Digito 2: "))
     if r == 's':
         res=n1+n2
+        print("Resultado de la operacion obtenido! ====> ")
+        return print(res)
     elif r == 'r' :
         res=n1-n2
+        print("Resultado de la operacion obtenido! ====> ")
+        return print(res)
     elif r == 'm' :
         res=n1*n2
+        print("Resultado de la operacion obtenido! ====> ")
+        return print(res)
     elif r == 'd' :
         res=n1/n2
-    return res
+        print("Resultado de la operacion obtenido! ====> ")
+        return print(res)
 
+   
 r= input("Operacion: ")
-print(calculadora(r))
+n1=int(input("Digito 1: "))
+calculadora(r)
