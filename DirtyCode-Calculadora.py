f6c9a2562949df563508450501fc559ce3c8d875
Yigael Balderrama <yigaelzero@gmail.com>
2021-03-27 19:21:35 -0400

    Refactor

--- DirtyCode-Calculadora.py
+++ DirtyCode-Calculadora.py
@@ -1,23 +1,20 @@
-def calculadora(r):
-    n2=int(input("Digito 2: "))
-    if r == 's':
-        res=n1+n2
-        print("Resultado de la operacion obtenido! ====> ")
-        return print(res)
-    elif r == 'r' :
-        res=n1-n2
-        print("Resultado de la operacion obtenido! ====> ")
-        return print(res)
-    elif r == 'm' :
-        res=n1*n2
-        print("Resultado de la operacion obtenido! ====> ")
-        return print(res)
-    elif r == 'd' :
-        res=n1/n2
-        print("Resultado de la operacion obtenido! ====> ")
-        return print(res)
-
-   
-r= input("Operacion: ")
-n1=int(input("Digito 1: "))
-calculadora(r)
+def extraerDatos(expresion):
+    return int(expresion[0]), int(expresion[2]), str(expresion[1])
+def calculadora(nro1,nro2,operador):
+    resultado = nro1
+    if operador =='+':
+        resultado+=nro2
+    elif operador == '-':
+        resultado-=nro2
+    elif operador == '*':
+        resultado*=nro2
+    elif operador == '/':
+        resultado/=nro2
+    else:
+        print("")
+        resultado=None
+    return resultado    
+print("Ingrese su Operacion:")
+operacion=input().split()
+Nro1,Nro2,operador=extraerDatos(operacion)
+print(f"El resultado es: {calculadora(Nro1,Nro2,operador)}")
---
+++ Refatored.md
@@ -0,0 +1,7 @@
+SmellCode --> REFACTORIZACION
+lineas 5,9,13,17 codigo repetido --> se elimino las lineas a solo una
+lineas 2,22,21 variables poco descriptivas --> se rescribieron el nombre de las variables
+linea 1 funcion multitask(procesa los datos,realiza la operacion)-->se dividio en dos funciones
+lineas 6,10,14,18 multiples returns --> se rescribio el codigo a un solo return linea 
+lineas 3,7,11,15 condicionales poco probables --> se rescribieron los ifs 
+lineas 3,7,11,15 no hay una condicional por defecto --> se añadio un condicional por defecto 
